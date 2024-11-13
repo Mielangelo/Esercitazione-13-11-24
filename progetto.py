d9d8168a6130b98c8fd39cea886eea48b94ea0cb
Mielangelo~ <angelomiele@hotmail.it>
2024-11-13 09:56:35 -0500

    Esercitazione-13-11-24

---
+++ progetto.py
@@ -0,0 +1,5 @@
+città=input("Inserisci il nome della tua città di origine: ")
+animale=input("Inserisci il nome del tuo animale domestico: ")
+
+nome_band = f"{città}{animale} Band"
+print(f"Il nome della tua band potrebbe essere: {nome_band}")
